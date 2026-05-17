97c0f51e3a2d9cddb78dbb99af65b7813935907f
Akash kumar <51863713+Akash-cool20@users.noreply.github.com>
2026-05-17 18:25:32 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int  can=nums[0],vote=1;
+        for(int i=1;i< nums.length;i++){
+            if(can == nums[i]){
+                vote++;
+            }
+            else{
+                vote--;
+            }
+            if(vote == 0){
+                can = nums[i];
+                vote = 1;
+            }
+        }
+        return can;
+    }
+}
